835c9bd5b5e3cf2643f03e38809358e5cba8224e
Lucas <lucasvarelanegro@gmail.com>
2022-03-17 18:15:28 +0100

    Primera Versión

---
+++ src/main/java/com/mycompany/proyecto_git/Sumador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto_git;
+
+/**
+ *
+ * @author lucas.varelanegro
+ */
+public class Sumador { 
+public static float sumar (float a, float b ) {
+return a + b;
+}
+}
